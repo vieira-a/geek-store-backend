154c92d5f01bb21e3b810024951f839c14005f3a
Anderson Vieira <asvieira.br@gmail.com>
2025-01-22 10:43:38 -0300

    feat: add MongoDB integration with configuration management

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongoDbModule } from './module/shared/persistence/mongodb/mongodb.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    MongoDbModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
