41cfee8b4de311b2dcb5895c1441870225f9a421
Anderson Vieira <asvieira.br@gmail.com>
2025-01-24 00:17:49 -0300

    feat: update CryptographyService to use configurable salt rounds for password hashing

--- src/module/shared/criptography/service/criptography.service.ts
+++ src/module/shared/criptography/service/criptography.service.ts
@@ -1,12 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 import { CryptographyInterface } from '../interface/criptography.interface';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class CryptographyService implements CryptographyInterface {
-  private readonly saltRounds = 10;
+  constructor(private readonly configService: ConfigService) {}
 
   async hash(value: string): Promise<string> {
-    return bcrypt.hash(value, this.saltRounds);
+    const saltRounds = parseInt(
+      this.configService.get<string>('ENCRYPT_SALT', '10'),
+      10,
+    );
+    return bcrypt.hash(value, saltRounds || 10);
   }
 }
