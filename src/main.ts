31cdc46ddb7a47400d41d18331c9e16dc69ec109
Anderson Vieira <asvieira.br@gmail.com>
2025-01-29 16:22:52 -0300

    feat: enhance ValidationPipe and update CORS methods for improved request handling

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { GlobalExceptionFilter } from './module/shared/exception/global-exception-filter';
 import { ValidationPipe } from '@nestjs/common';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { setupSwagger } from './module/shared/swagger/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalFilters(new GlobalExceptionFilter());
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: false,
+    }),
+  );
 
   app.enableCors({
     origin: process.env.CORS_ORIGIN ?? '*',
-    methods: 'GET,POST,PUT,DELETE',
+    methods: 'GET,POST,PUT, PATCH, DELETE',
     allowedHeaders: 'Content-Type, Authorization',
   });
 
